ae7f24ed121cdd4924d11bdf4800cd03219246b4
MinassieE <menaephrem17@gmail.com>
2024-03-01 16:37:29 +0300

    todo list functionality

--- todolist/app.js
+++ todolist/app.js
@@ -5,9 +5,26 @@ const urlencoded = require("body-parser/lib/types/urlencoded");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const today = new Date();
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+}
+
+let day = today.toLocaleDateString("en-us", options);
+let items = ["todo1", "todo2", "todo3"];
 
 app.get("/", (req, res)=>{
-    res.render('list');
+    res.render('list', {kindOfDay: day, newListItems: items});
+})
+app.post("/", (req, res)=>{
+    item = req.body.newItem;
+    items.push(item);
+    console.log(item);
+    res.redirect("/");
 })
 
 app.listen(3000, ()=>{
