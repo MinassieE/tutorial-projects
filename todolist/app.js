5e7f0e2e410b7b5431c57dceab7407eae79460ce
MinassieE <menaephrem17@gmail.com>
2024-05-01 22:38:51 +0300

    template and layout

--- todolist/app.js
+++ todolist/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 const today = new Date();
 let options = {
@@ -16,15 +17,30 @@ let options = {
 
 let day = today.toLocaleDateString("en-us", options);
 let items = ["todo1", "todo2", "todo3"];
+let workItems = [];
 
 app.get("/", (req, res)=>{
-    res.render('list', {kindOfDay: day, newListItems: items});
+    res.render('list', {ListTitle: day, newListItems: items});
 })
 app.post("/", (req, res)=>{
     item = req.body.newItem;
-    items.push(item);
-    console.log(item);
-    res.redirect("/");
+    
+    console.log(req.body);
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/work", (req, res)=>{
+    res.render("list", {ListTitle: "Work", newListItems: workItems});
+})
+app.get("/about", (req, res)=>{
+    res.render("about");
 })
 
 app.listen(3000, ()=>{
